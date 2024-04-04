68e8a3d3fb4b1017f55490c4dcfa250ffc0f05a8
Dauirzhan2002 <104432303+Dauirzhan2002@users.noreply.github.com>
2024-04-04 14:34:14 +0200

    changing internal values to public in class Client attempt at creating GetById_ThrowsExceptionWhenNoID()

--- LegacyApp.tests/UserServiceTests.cs
+++ LegacyApp.tests/UserServiceTests.cs
@@ -18,7 +18,7 @@ public class UserServiceTests
         
         // Assert
         Assert.False(result);
-
+        
     }
     [Fact]
     public void AddUsers_ThrowsExceptionWhenClientDoesntExist()
@@ -36,6 +36,29 @@ public class UserServiceTests
         
         // Assert
         Assert.Throws<ArgumentException>(action);
+        
+    }
 
+    public void GetById_ThrowsExceptionWhenNoID()
+    {
+        
+        // Arrange
+        
+        Dictionary<int, Client> database = new Dictionary<int, Client>()
+        {
+            {1, new Client{ClientId = 1, Name = "Kowalski", Address = "Warszawa, Złota 12", Email = "kowalski@wp.pl", Type = "NormalClient"}},
+            {2, new Client{ClientId = 2, Name = "Malewski", Address = "Warszawa, Koszykowa 86", Email = "malewski@gmail.pl", Type = "VeryImportantClient"}},
+            {3, new Client{ClientId = 3, Name = "Smith", Address = "Warszawa, Kolorowa 22", Email = "smith@gmail.pl", Type = "ImportantClient"}},
+            {4, new Client{ClientId = 4, Name = "Doe", Address = "Warszawa, Koszykowa 32", Email = "doe@gmail.pl", Type = "ImportantClient"}},
+            {5, new Client{ClientId = 5, Name = "Kwiatkowski", Address = "Warszawa, Złota 52", Email = "kwiatkowski@wp.pl", Type = "NormalClient"}},
+            {6, new Client{ClientId = 6, Name = "Andrzejewicz", Address = "Warszawa, Koszykowa 52", Email = "andrzejewicz@wp.pl", Type = "NormalClient"}}
+        };
+
+        // Act
+        //Action action = () => ClientRepository.GetById(399);
+        
+        //Assert
+        
+        //Assert.Throws<ArgumentException>(action);
     }
 }
